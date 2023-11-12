483d346680af22be0c5e3e45a35e05a7cd8cba47
DavidHaring20 <davidharingri@gmail.com>
2023-11-12 14:39:14 +0100

    Created products migration and model.

---
+++ app/Models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    const CREATED_AT = 'published_date';
+    const UPDATED_AT = null;
+    protected $primaryKey = 'product_sku';
+    protected $keyType = 'string';
+    public $incrementing = false;
+}
---
+++ database/migrations/2023_11_12_122848_create_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->char('product_sku', 8);
+            $table->char('name', 50);
+            $table->char('description', 250);
+            $table->decimal('price', $precision=7, $scale=2);
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
