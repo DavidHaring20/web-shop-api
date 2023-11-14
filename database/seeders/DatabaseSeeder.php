06c8840202bce46f9b6618eef14a04b22383e1bc
DavidHaring20 <davidharingri@gmail.com>
2023-11-14 13:25:25 +0100

    Created CategorySeeder and CategoryFactory. Included category in DatabaseSeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\Product::factory(10)->create();
+        \App\Models\Category::factory(2)->create();
     }
 }
